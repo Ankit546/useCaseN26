1308f91e65019767378ed40a1157ce72b492fc01
Kumar Ankit <ku.ankit1997@gmail.com>
2023-03-07 01:34:37 +0530

    N26 Use Case for LWC
    Please note this is the LWC Component folder. The Apex class is present in the root folder.

---
+++ caseProductN26/caseProductN26.js
@@ -0,0 +1,26 @@
+import { LightningElement, api, wire } from 'lwc';
+import fetchProductsFromContacts from '@salesforce/apex/fetchProductsFromContactsToCase.fetchProductsFromContacts';
+
+import fetchCustomerProducts from '@salesforce/apex/fetchProductsFromContactsToCase.fetchCustomerProducts';
+
+const columns = [
+    { label: 'Product Name', fieldName: 'Name' },
+    { label: 'Product ATM Fee', fieldName: 'ATM_Fee_in_Other_Currencies__c' },
+    { label: 'Country', fieldName: 'Country__c'},
+    { label: 'Card Replacement Cost', fieldName: 'Card_Replacement_Cost__c' },
+    { label: 'Cost Per Month', fieldName: 'Cost_Per_Calendar_Month__c' }
+];
+
+export default class CaseProductN26 extends LightningElement {
+
+    @api recordId;
+    columns = columns;
+
+    @wire(fetchProductsFromContacts, {recordId : '$recordId'})
+    contactsproducts;
+
+    @wire(fetchCustomerProducts, {recordId : '$recordId'})
+    customerproducts;
+
+
+}
